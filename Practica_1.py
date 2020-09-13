17a25a6d05c37348d4dace02f2fe10ba0c04c3b6
Luis-Emmanuel <67926526+Luis-Emmanuel@users.noreply.github.com>
2020-09-13 16:51:40 -0500

    Update Practica_1.py

--- Practica_1.py
+++ Practica_1.py
@@ -148,6 +148,6 @@ plt.show()
 
 # In[ ]:
 
-
-
+'''
+Nota: debido a falta de tiempo por otros deberes y falta de experiencia con el manejo de funciones con python ya no me fue posible cumplir con la parte 4 de la practica.
 
